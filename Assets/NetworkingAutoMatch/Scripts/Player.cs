4b52e4261840083b7b25d1bfe660154acee1a2c2
Tim Graupmann <tgraupmann@gmail.com>
2016-12-04 10:25:26 -0800

    Display who is the client and server

--- Assets/NetworkingAutoMatch/Scripts/Player.cs
+++ Assets/NetworkingAutoMatch/Scripts/Player.cs
@@ -24,13 +24,25 @@ namespace NetworkingAutoMatch
 
         private Color[] _mColors =
         {
-        Color.red,
-        Color.green,
-        Color.blue,
-        Color.yellow,
-        Color.cyan,
-        Color.magenta,
-    };
+            Color.red,
+            Color.green,
+            Color.blue,
+            Color.yellow,
+            Color.cyan,
+            Color.magenta,
+        };
+
+        public void OnGUI()
+        {
+            if (isLocalPlayer)
+            {
+                GUILayout.BeginHorizontal(GUILayout.Width(Screen.width));
+                GUILayout.FlexibleSpace();
+                GUILayout.Label(string.Format("You are the {0}", isServer ? "SERVER" : "CLIENT"));
+                GUILayout.FlexibleSpace();
+                GUILayout.EndHorizontal();
+            }
+        }
 
         private void HidePlayer()
         {
@@ -100,8 +112,7 @@ namespace NetworkingAutoMatch
             }
         }
 
-        [Command]
-        public void CmdFire()
+        private void Fire()
         {
             // Create the Bullet from the Bullet Prefab
             GameObject bullet = (GameObject)Instantiate(_mBulletPrefab, _mBulletSpawn.position, _mBulletSpawn.rotation);
@@ -114,6 +125,35 @@ namespace NetworkingAutoMatch
 
             // Destroy the bullet after 2 seconds
             Destroy(bullet, 2.0f);
+        }
+
+        /// <summary>
+        /// ClientRpc calls are sent from objects on the server to objects on clients.
+        /// They can be sent from any server object with a NetworkIdentity that has
+        /// been spawned. Since the server has authority, then there no security
+        /// issues with server objects being able to send these calls. To make a
+        /// function into a ClientRpc call, add the [ClientRpc] custom attribute to
+        /// it, and add the “Rpc” prefix. This function will now be run on clients
+        /// when it is called on the server. Any arguments will automatically be
+        /// passed to the clients with the ClientRpc call.
+        /// ClientRpc functions must have the prefix “Rpc”. This is a hint when
+        /// reading code that calls the method - this function is special and is
+        /// not invoked locally like a normal function.
+        /// </summary>
+        [ClientRpc]
+        public void RpcFire()
+        {
+            Fire();
+        }
+
+        /// <summary>
+        /// The [Command] attribute indicates that the following function
+        /// will be called by the Client, but will be run on the Server.
+        /// </summary>
+        [Command]
+        public void CmdFire()
+        {
+            Fire();
         }
 
         // Update is called once per frame
